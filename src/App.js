12d9d8e001690dd33e7ee71cdfc3753082610c40
patthamawa <patthama.wa.65@ubu.ac.th>
2022-07-25 11:20:12 +0700

    add

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import React, {useState} from 'react';
 
 function App() {
   const [num, setNum] = useState(0); 
+  const [primeR, setPrime] = useState("");
 
   function AddPrimeNumber(){
     let prime = true;
-    setNum(num + 1);
-    for (let i = 2; i <num; i++) {
-        if (num % i == 0) {
+    let num_x = num + 1;
+    setNum(num_x);
+    if (num_x == 0 || num_x == 1) setPrime("ไม่เป็นจำนวนเฉพาะ")
+    else {
+    for (let i = 2; i <num_x; i++) {
+        if (num_x % i == 0) {
           prime = false;
         break;
     }
   }
-    let r = "";
-    if (prime) r = "{num}เป็นจำนวนเฉพาะ";
-    else r = "{num}ไม่เป็นจำนวนเฉพาะ";
-    console.log(num,r);
-    return r;
+    if (prime) setPrime ("เป็นจำนวนเฉพาะ");
+    else setPrime ("ไม่เป็นจำนวนเฉพาะ");
   }
+}
 
   function SubtractPrimeNumber() {
     return setNum(num-1);
@@ -32,6 +34,7 @@ function App() {
         <h2>{num}</h2>
         <button className='btn btn-primary' onClick={() => setNum(num+1)}>เพิ่ม</button>&nbsp;
         <button className='btn btn-danger'onClick={() => setNum(num-1)}>ลด</button>
+        <p>{num} {primeR}</p>
       </div>
       <MyFooter />
     </div>
